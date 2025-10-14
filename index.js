108d04f2c5bd47ad849f9d1c6061bded793cf41e
darkwuty <magiksith@gmail.com>
2025-10-14 14:10:51 +0200

    mvc newsletter

--- index.js
+++ index.js
@@ -21,9 +21,11 @@ app.use(express.static(path.join(__dirname, "/src/Public")));
 const routeAdmin = require("./src/Route/admin.route");
 const routePublic = require("./src/Route/public.route");
 const routeAuth = require("./src/Route/auth.route");
+const routeNewsletter = require("./src/Route/newsletterRoutes.js");
 
 app.use("/admin", routeAdmin);
 app.use("/auth", routeAuth);
+app.use("/newsletters", routeNewsletter);
 // app.use("/", routePublic);
 
 // -------------- //
